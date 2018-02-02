80ca3531ed563ed8a863f9a3e5ed7b3a03c45b13
Raju Kumar <rakumar@csumb.edu>
2018-02-02 06:19:35 +0000

    Added lab 1 files

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8" />
+        <title> Raju Kumar: Personal Website </title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    </head>
+<!-- closing head -->
+    <!-- This is the body -->
+    <body>
+        <header>
+            <h1> Raju Kumar</h1>
+        </header>
+        <nav>
+            <hr width="50%" />
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+        
+        <br /><br />
+        <!-- This is the footer -->
+        <main>
+            
+            
+            <figure id="me">
+                <img src="img/juan_doe.png" alt="My profile Picture" />
+            </figure>
+            
+            
+            <div id="welcomeText">
+                <aside>
+                Hello! <br/>
+                <p>
+                    Thank you for visiting my website. </p>
+                    <p>I am a 3rd year Computer Science student @ CSU Montery Bay.</p>
+                    
+                    <p> Feel free to contact me!</p>
+                    
+                    <br /><br />
+                    
+                    <em>"Facts are
+                    <strong> meaningless.</strong> You could use facts to prove anything that's even remotely true."</em><br />
+                    -Homer J. Simpson
+                    </aside>
+                </p>
+            </div>
+            
+            
+        </main>
+    
+        <footer>
+            <hr>
+            CST-336: Internet Programming. &copy2018; Kumar <br />
+            <strong>Disclaimer:</strong> The information in this webpage
+            is fictitous. <br />
+            It is used for academic purposes only.
+            
+            
+            <figure id="csumb">
+                <img src="img/csumb.png" alt="Picute of CSUMB" width = "300" height = "120"/>
+            </figure>
+            
+        </footer>
+        <!-- closing footer -->
+        
+        
+    </body>
+    <!-- closing body -->
+
+</html>
